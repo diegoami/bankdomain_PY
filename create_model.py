36b1032efb6f36544167efc1a8cf7a113a7c2bc9
Diego Amicabile <diego.amicabile@gmail.com>
2018-05-03 01:02:06 +0200

    Some other fine-tuning

--- create_model.py
+++ create_model.py
@@ -2,7 +2,7 @@
 import yaml
 from repository.mongo_ops import copy_into_qa_documents, split_qa_documents_into_questions, print_all_questions, iterate_questions_in_mongo, iterate_proc_questions_in_mongo, process_questions, question_for_model
 from preprocess.txt_preprocess import create_corpus, load_corpus, print_corpus,preprocess_text, model_process
-from components.custom_lemmas import CUSTOM_LOOKUP
+from components.custom_lemmas import CUSTOM_LOOKUP, CUSTOM_REMOVES
 from textacy.corpus import Corpus
 import spacy
 if __name__ == '__main__':
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     corpus_mod_filename = config['corpus_mod_filename']
 
     nlp = spacy.load('de')
+    map(nlp.Defaults.lemma_lookup.pop, CUSTOM_REMOVES)
     nlp.Defaults.lemma_lookup.update(CUSTOM_LOOKUP)
     all_texts = question_for_model(mongo_connection, model_process, nlp)
     with open('data/mod_questions.txt', 'w') as f:
