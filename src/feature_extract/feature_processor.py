8a6c6a988e1496451d68e3211bfa06b3804b086b
Diego Amicabile <diego.amicabile@gmail.com>
2018-05-05 15:06:59 +0200

    Integrated gensim into stuff

---
+++ src/feature_extract/feature_processor.py
@@ -0,0 +1,74 @@
+from .feature_consts import *
+from common import replace_strings
+
+class FeatureProcessor:
+
+    def __init__(self, nlp):
+        self.nlp = nlp
+
+
+    def __call__(self, text):
+        text = self.replace_concepts(text)
+        text = self.model_process(text)
+
+        return text
+
+    def replace_concepts(self, text):
+        text = replace_strings(text, month_names, month_name)
+        text = IBAN_REGEX.sub('IBAN', text)
+        return text
+
+    def model_process(self, text):
+
+        doc = self.nlp(text)
+        docl = self.nlp(text.lower())
+        curr_trunc = None
+        keep_toks = []
+        for index, token in enumerate(doc):
+            if (token.is_stop):
+                pass
+            elif (index < len(docl) and docl[index].is_stop):
+                pass
+            elif any([regex.match(token.text) for regex in REGEXES ]):
+                pass
+            elif (token.text in PUNKT_PREPROCESS):
+                pass
+            elif (token.pos_ in POS_IGNORE):
+                pass
+            elif curr_trunc:
+                found_ende = [p for p in possible_integrator if token.lemma_.lower().endswith(p)]
+                if (len(found_ende) > 0):
+
+                    keep_toks.append(curr_trunc+found_ende[0])
+                else:
+                    keep_toks.append(curr_trunc)
+                keep_toks.append(token.lemma_)
+                curr_trunc = None
+            else:
+                if (token.tag_ == "TRUNC" and token.text[-1] == '-'):
+                    curr_trunc = token.text[:-1]
+                elif token.pos_ == "VERB":
+                    sep_part = [x for x in token.children if x.tag_ == "PTKVZ"
+                                and x.text in GERMAN_SEPARABLE ]
+                    if (len(sep_part) > 0):
+                        to_app = sep_part[0].text+token.lemma_.lower()
+
+                        keep_toks.append(to_app)
+                    else:
+                        keep_toks.append(token.lemma_)
+
+                elif index < len(docl) and docl[index].pos_ == "VERB" and any([x for x in docl[index].children if x.tag_ == "PTKVZ" and x.text in GERMAN_SEPARABLE ]):
+                    sep_part = [x for x in docl[index].children if x.tag_ == "PTKVZ"
+                                and x.text in GERMAN_SEPARABLE]
+                    if (len(sep_part) > 0):
+                        to_app = sep_part[0].text + docl[index].lemma_.lower()
+
+                        keep_toks.append(to_app)
+                elif (index < len(docl)      and docl[index].pos_ == "VERB") and (docl[index].lemma_ != docl[index].text):
+                    keep_toks.append(docl[index].lemma_)
+                else:
+                    keep_toks.append(token.lemma_)
+
+
+        return " ".join(keep_toks)
+
---
+++ src/preprocess/preprocessor.py
@@ -0,0 +1,31 @@
+
+from .lists import *
+from textacy.preprocess import preprocess_text
+
+from common import map_products, replace_strings
+
+
+class Preprocessor:
+    def __call__(self, text):
+        return self.custom_preprocess(text)
+
+    def replace_bank_names(self, text):
+        text = map_products(text, products_map)
+        text = replace_strings(text, first_banks, first_bank_name)
+        text = replace_strings(text, second_banks, second_bank_name)
+        text = replace_strings(text, companies, company_name)
+        text = replace_strings(text, countries, country)
+        text = replace_strings(text, towns, town)
+        return text
+
+    def replace_characters_to_space(self, text):
+        for index, ctos in enumerate(characters_to_space):
+            text = text.replace(ctos , characters_spaced[index])
+        return text
+
+
+    def custom_preprocess(self, text):
+        text = self.replace_bank_names(text)
+        text = preprocess_text(text, fix_unicode = True, lowercase = False, no_urls = True, no_emails = True, no_phone_numbers = True, no_punct = False, no_numbers=False)
+        text = self.replace_characters_to_space(text)
+        return text
