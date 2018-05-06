16f1a370be1893e590534a03540cce51cc55aeab
Diego Amicabile <diego.amicabile@gmail.com>
2018-05-06 23:43:07 +0200

    nationalities added

--- src/preprocess/lists/__init__.py
+++ src/preprocess/lists/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['companies', 'company_name', 'countries', 'country',
            'first_bank_name', 'first_banks', 'second_banks' , 'second_bank_name', 'towns' , 'town', 'products_map',
-           'characters_to_space', 'characters_spaced']
+           'characters_to_space', 'characters_spaced', 'nationalities', 'nationality']
 
 from .companies import companies, company_name
 from .countries import countries, country
@@ -9,3 +9,4 @@ from .second_banks import second_bank_name, second_banks
 from .products_map import products_map
 from .towns import town, towns
 from .prep_characters import characters_to_space, characters_spaced
+from .nationalities import nationalities, nationality
