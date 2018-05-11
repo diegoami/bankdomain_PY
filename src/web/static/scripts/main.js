6db56e78b124f53e3e3f5bcc6353bdc06a40785f
Diego Amicabile <diego.amicabile@gmail.com>
2018-05-11 23:24:23 +0200

    Decent look and feel of the application

--- src/web/static/scripts/main.js
+++ src/web/static/scripts/main.js
@@ -1,3 +1,11 @@
+select_header = function(element) {
+    var topNav_element = document.getElementById(element);
+    topNav_element.setAttribute("class", "active");
+}
+
+
+
+
 
 function move_to_page(page_id, form) {
     var mainForm = document.getElementById(form);
