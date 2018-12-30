35f410ed84f836dfc3728435122bf46d4cd45c98
diegoami <diego.amicabile@gmail.com>
2018-12-30 10:25:37 +0100

    import scripts

--- scripts/import_mongo.sh
+++ scripts/import_mongo.sh
@@ -3,10 +3,10 @@ BDIR=/media/diego/QData/bankdomain/dump/
 rm -rf $BDIR
 mkdir -p $BDIR
 rm $BFILE
-aws s3 cp s3://bankdomain/$1 $BFILE
+aws s3 cp $1 $BFILE
 tar xvf $BFILE -C /
 cd /media/diego/QData/bankdomain/
-mongorestore
+mongorestore --drop
 
 
 
